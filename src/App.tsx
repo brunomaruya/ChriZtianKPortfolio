44d50ebc76dbe5d59c55e3fd374cdb6d1d1a0e87
Bruno <brun0yuj1ym02@gmail.com>
2023-01-03 01:09:18 +0900

    bugs solved

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ import { Contact } from './pages/Contact';
 import { ErrorPage } from './pages/ErrorPage/indes';
 import { Home } from './pages/Home';
 import { Portfolio } from './pages/Portfolio';
+import { Portfolio2 } from './pages/Portfolio2';
 import { AppContainer, GlobaStyles } from './styles/GlobalStyles';
 import { defaultTheme } from './styles/themes/default';
 function App() {
@@ -29,6 +30,7 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
             <Route path="/portfolio" element={<Portfolio />} />
+
             <Route path="/contact" element={<Contact />} />
           </Routes>
 
--- src/firebase.ts
+++ src/firebase.ts
@@ -1,20 +1,20 @@
+import { getStorage } from 'firebase/storage';
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
 import { getAnalytics } from 'firebase/analytics';
-import { getStorage } from 'firebase/storage';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: 'AIzaSyCuRtAO17vMSou57GVFEAC1tzHEitdDawY',
-  authDomain: 'chriztiank-92a86.firebaseapp.com',
-  projectId: 'chriztiank-92a86',
-  storageBucket: 'chriztiank-92a86.appspot.com',
-  messagingSenderId: '783771587054',
-  appId: '1:783771587054:web:63ec62c5321c5d8b253eff',
-  measurementId: 'G-KT7J3TD376',
+  apiKey: 'AIzaSyDmJ83l-edggRetWYbZ4GAFT8FJwC3UlrA',
+  authDomain: 'chriztiankv2.firebaseapp.com',
+  projectId: 'chriztiankv2',
+  storageBucket: 'chriztiankv2.appspot.com',
+  messagingSenderId: '658589884797',
+  appId: '1:658589884797:web:018283472f616bd1d76c14',
+  measurementId: 'G-HDKCDXMT3B',
 };
 
 // Initialize Firebase
